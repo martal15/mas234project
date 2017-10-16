5246793ba5cf0467e587d40a68de1af11b4395b6
Benjamin <dev@booleanbyte.com>
2017-10-16 16:18:54 +0200

    1.5 _ LED blinker

--- mas234_project/MAS234lab.cpp
+++ mas234_project/MAS234lab.cpp
@@ -1,9 +1,18 @@
+#define F_CPU 1000000L
+
+#include <avr/io.h>
+#include <util/delay.h>
 
 int main(void)
 {
-
+	DDRD = (1 << PD0);
+	PORTD = 0x00;
+	
 	while(1)
 	{
-		
+		PORTD = (1 << PD0);
+		_delay_ms(500);
+		PORTD = (0 << PD0);
+		_delay_ms(500);
 	}
 }
